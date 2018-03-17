ce0bf3eb9926a9a79bb1bbd2062887fe91e0ba7d
Karol Świerczek <karol.sw3@gmail.com>
2018-03-17 21:36:00 +0100

    Make canvas and ctx variables global

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
 class Game {
 
   constructor () {
-    this.canvas = document.getElementById('canvas')
-    this.ctx = this.canvas.getContext('2d')
     this.board = new Board(40, 40)
     window.addEventListener('resize', this.resize)
     this.resize()
-
-    this.show = this.show.bind(this)
-
-    this.show()
+    this.draw = this.draw.bind(this)
+    this.draw()
   }
 
   resize () {
-    this.canvas.width = window.innerWidth
-    this.canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
   }
 
-  show () {
-    this.ctx.beginPath()
-    this.ctx.rect(0, 0, this.canvas.width, this.canvas.height)
-    this.ctx.fillStyle = 'black'
-    this.ctx.fill()
+  draw () {
+    ctx.beginPath()
+    ctx.rect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'black'
+    ctx.fill()
+
+    this.board.draw()
   }
 
 }
@@ -31,6 +29,10 @@ class Board {
   constructor (sizeX, sizeY) {
     this.sizeX = sizeX
     this.sizeY = sizeY
+  }
+
+  draw () {
+    
   }
 
 }
@@ -44,4 +46,6 @@ class Snake {
 
 }
 
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
 var game = new Game()
